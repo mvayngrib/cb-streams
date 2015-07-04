1a2fb101b38872c87a7784640c53a6ee25576bf3
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-07-03 20:25:01 -0400

    add forgotten files

---
+++ index.js
@@ -0,0 +1,42 @@
+var blockstream = require('./lib/blockstream')
+var toTxs = require('./lib/toTxs')
+var dataTxsOnly = require('./lib/dataTxsOnly')
+var txsForAddresses = require('./lib/txsForAddresses')
+var getData = require('./lib/getData')
+var combine = require('stream-combiner2')
+
+function txstream (options) {
+  return options.addresses ? txsForAddresses(options) : combine(
+    blockstream(options),
+    toTxs()
+  )
+}
+
+function datatxstream (options) {
+  return combine(
+    txstream(options),
+    dataTxsOnly()
+  )
+}
+
+function datastream (options) {
+  return combine(
+    datatxstream(options),
+    getData()
+  )
+}
+
+module.exports = {
+  stream: {
+    blocks: blockstream,
+    txs: txstream,
+    dataTxs: datatxstream,
+    data: datastream
+  },
+  filter: {
+    data: dataTxsOnly
+  },
+  map: {
+    data: getData
+  }
+}
